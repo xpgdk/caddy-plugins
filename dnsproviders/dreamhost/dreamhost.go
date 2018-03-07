09edd094dba204f334bd2b03766642d0904db0ed
Paul Fleischer <paul@xpg.dk>
2018-03-07 23:23:28 +0100

    Add dreamhost DNS provider

---
+++ dnsproviders/dreamhost/dreamhost.go
@@ -0,0 +1,30 @@
+// Package dnsimple adapts the lego DNS Made Easy DNS provider
+// for Caddy. Importing this package plugs it in.
+package dreamhost
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/dreamhost"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("dreamhost", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new DNS Made Easy DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use credentials from environment
+// len(1): credentials[0] = API key
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return dreamhost.NewDNSProvider()
+	case 1:
+		return dreamhost.NewDNSProviderCredentials(credentials[0])
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
